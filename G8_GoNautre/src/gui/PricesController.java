fc2012c557714cd2068bb1e9abe7113d83393b81
Shlomi <shlomia1074@gmail.com>
2020-12-15 18:20:50 +0200

    add prices

---
+++ G8_GoNautre/src/gui/PricesController.java
@@ -0,0 +1,20 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class PricesController implements Initializable {
+	  @FXML
+	    private Label lblPrice;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+
+}
