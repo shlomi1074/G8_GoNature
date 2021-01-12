599381993f3e5d355ee1facbea58f4217f21c7cc
liork <liork@LAPTOP-T0OA804P>
2021-01-12 18:08:08 +0200

    **Lior - notes on tests for JUnitTest HW**

--- G8_Testing/UnitTests/UnitTest/EntranceReportServerSide.java
+++ G8_Testing/UnitTests/UnitTest/EntranceReportServerSide.java
@@ -14,6 +14,7 @@ public class EntranceReportServerSide {
 	private static Connection mysqlconnection;
 	private ReportsQueries dbQueries;
 	private ArrayList<String> parameters;
+
 	
 	@Before
 	public void setUp() throws Exception {
@@ -22,6 +23,11 @@ public class EntranceReportServerSide {
 		parameters = new ArrayList<>();
 	}
 
+	/**
+	 * This test checks the count of solo in order to check CountSolosEnterTime query
+	 * input: "1"
+	 * expected result: 10
+	 */
 	@Test
 	public void CountSolosEnterTimeTest() {
 		parameters.add("1");
@@ -30,6 +36,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of solo in an empty month in order to check CountSolosEnterTime query
+	 * input: "2"
+	 * expected result: 0
+	 */
 	@Test
 	public void CountSolosEnterTimeEmptyMonthTest() {
 		parameters.add("2");
@@ -38,6 +49,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of solo when connection failed in order to check CountSolosEnterTime exception
+	 * input: "2" , dbQueries = ReportsQueries(null)
+	 * expected result: null
+	 */
 	@Test
 	public void CountSolosEnterTimeFailedSQLTest() {
 		dbQueries = new ReportsQueries(null);
@@ -46,6 +62,11 @@ public class EntranceReportServerSide {
 		assertEquals(null, actual);
 	}
 	
+	/**
+	 * This test checks the count of subscriber in order to check CountSubsEnterTime query
+	 * input: "1"
+	 * expected result: 13
+	 */
 	@Test
 	public void CountSubsEnterTimeTest() {
 		parameters.add("1");
@@ -54,6 +75,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of subscriber in an empty month in order to check CountSubsEnterTime query
+	 * input: "2"
+	 * expected result: 0
+	 */
 	@Test
 	public void CountSubsEnterTimeEmptyMonthTest() {
 		parameters.add("2");
@@ -62,6 +88,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of subscriber when connection failed in order to check CountSubsEnterTime exception
+	 * input: "2" , dbQueries = ReportsQueries(null)
+	 * expected result: null
+	 */
 	@Test
 	public void CountSubsEnterTimeFailedSQLTest() {
 		dbQueries = new ReportsQueries(null);
@@ -70,6 +101,11 @@ public class EntranceReportServerSide {
 		assertEquals(null, actual);
 	}
 	
+	/**
+	 * This test checks the count of group in order to check CountGroupsEnterTime query
+	 * input: "1"
+	 * expected result: 22
+	 */
 	@Test
 	public void CountGroupsEnterTimeTest() {
 		parameters.add("1");
@@ -78,6 +114,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of group in an empty month in order to check CountGroupsEnterTime query
+	 * input: "2"
+	 * expected result: 0
+	 */
 	@Test
 	public void CountGroupsEnterTimeEmptyMonthTest() {
 		parameters.add("2");
@@ -86,6 +127,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of group when connection failed in order to check CountGroupsEnterTime exception
+	 * input: "2" , dbQueries = ReportsQueries(null)
+	 * expected result: null
+	 */
 	@Test
 	public void CountGroupsEnterTimeFailedSQLTest() {
 		dbQueries = new ReportsQueries(null);
@@ -93,7 +139,11 @@ public class EntranceReportServerSide {
 		ArrayList<VisitReport> actual = dbQueries.CountGroupsEnterTime(parameters);
 		assertEquals(null, actual);
 	}
-	
+	/**
+	 * This test checks the count of solo in specific day order to check CountSolosEnterTimeWithDays query
+	 * input: day = "1" , month = "1"
+	 * expected result: 1
+	 */
 	@Test
 	public void CountSolosEnterTimeWithDaysTest() {
 		parameters.add("1");
@@ -103,6 +153,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of solo in empty specific day order to check CountSolosEnterTimeWithDays query
+	 * input: day = "5" , month = "1"
+	 * expected result: 0
+	 */
 	@Test
 	public void CountSolosEnterTimeWithDaysEmptyDayTest() {
 		parameters.add("1");
@@ -112,6 +167,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of solo in specific day when connection failed in order to check CountSolosEnterTimeWithDays exception
+	 * input: day = "2" , month = "2" , dbQueries = ReportsQueries(null)
+	 * expected result: null
+	 */
 	@Test
 	public void CountSolosEnterTimeWithDaysFailedSQLTest() {
 		dbQueries = new ReportsQueries(null);
@@ -121,6 +181,11 @@ public class EntranceReportServerSide {
 		assertEquals(null, actual);
 	}
 	
+	/**
+	 * This test checks the count of subscriber in specific day order to check CountSubsEnterTimeWithDays query
+	 * input: day = "4" , month = "1"
+	 * expected result: 3
+	 */
 	@Test
 	public void CountSubsEnterTimeWithDaysTest() {
 		parameters.add("1");
@@ -130,6 +195,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of subscriber in empty specific day order to check CountSubsEnterTimeWithDays query
+	 * input: day = "5" , month = "1"
+	 * expected result: 0
+	 */
 	@Test
 	public void CountSubsEnterTimeWithDaysEmptyDayTest() {
 		parameters.add("1");
@@ -139,6 +209,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of subscriber in specific day when connection failed in order to check CountSubsEnterTimeWithDays exception
+	 * input: day = "2" , month = "2" , dbQueries = ReportsQueries(null)
+	 * expected result: null
+	 */
 	@Test
 	public void CountSubsEnterTimeWithDaysFailedSQLTest() {
 		dbQueries = new ReportsQueries(null);
@@ -148,6 +223,11 @@ public class EntranceReportServerSide {
 		assertEquals(null, actual);
 	}
 	
+	/**
+	 * This test checks the count of group in specific day order to check CountGroupsEnterTimeWithDays query
+	 * input: day = "6" , month = "1"
+	 * expected result: 11
+	 */
 	@Test
 	public void CountGroupsEnterTimeWithDaysTest() {
 		parameters.add("1");
@@ -156,7 +236,11 @@ public class EntranceReportServerSide {
 		int expected = 11;
 		assertEquals(expected, actual.size());
 	}
-	
+	/**
+	 * This test checks the count of group in empty specific day order to check CountGroupsEnterTimeWithDays query
+	 * input: day = "5" , month = "1"
+	 * expected result: 0
+	 */
 	@Test
 	public void CountGroupsEnterTimeWithDaysEmptyDayTest() {
 		parameters.add("1");
@@ -166,6 +250,11 @@ public class EntranceReportServerSide {
 		assertEquals(expected, actual.size());
 	}
 	
+	/**
+	 * This test checks the count of group in specific day when connection failed in order to check CountGroupsEnterTimeWithDays exception
+	 * input: day = "2" , month = "2" , dbQueries = ReportsQueries(null)
+	 * expected result: null
+	 */
 	@Test
 	public void CountGroupsEnterTimeWithDaysFailedSQLTest() {
 		dbQueries = new ReportsQueries(null);
